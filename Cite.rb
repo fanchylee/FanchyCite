7850a48032352f0e21c7bf0f85e053124a132d85
fanchylee <li380157578@sina.cn>
2013-02-01 01:48:47 +0800

    add cnki journal

--- Cite.rb
+++ Cite.rb
@@ -3,24 +3,55 @@ require 'net/http'
 require 'htmlentities'
 
 module Cite
-  class ISBN
+	class ISBN
 
-    def initialize(isbn)
-      @isbn=isbn.gsub(/-/,'')
-    end
+		def initialize(isbn)
+			@isbn=isbn.gsub(/-/,'')
+		end
 
-    def get_nlc()
-      params = {:func=>'find-m',:request=>@isbn,:find_code=>'ISB'}
-      uri = URI.parse('http://opac.nlc.gov.cn/F')
-      uri.query=URI.encode_www_form(params)
-      response =  Net::HTTP.get_response(uri)
+		def get
+			params = {:func=>'find-m',:request=>@isbn,:find_code=>'ISB'}
+			uri = URI.parse('http://opac.nlc.gov.cn/F')
+			uri.query=URI.encode_www_form(params)
+			response=Net::HTTP.get_response(uri)
 
-      content=HTMLEntities.new.decode( response.body.force_encoding("UTF-8"))
-      #content=HTMLEntities.new.decode(File.read('example').force_encoding("UTF-8"))
-      ta=/题名与责任.+?<\/td>.+?<td.+?>.+?>.+?>[[:blank:]]*(?<t>.+?)[[:blank:]]*\[.+?\].+?\/[[:blank:]]*(?<a>.+?)[[:blank:]]*[著编]*<.+?<\/td>/m.match(content)
-      pub=/出版项.+?<\/td>.+?<td.+?>.+?>.+?>[[:blank:]]*(?<l>.+?)[[:blank:]]*:[[:blank:]]*(?<p>.+?)[[:blank:]]*,[[:blank:]]*(?<y>.+?)[[:blank:]]*<.+?<\/td>/m.match(content)
-      isbn=/Z13_ISBN_ISSN.+?value=\"(.+?)[[:blank:]]+/.match(content)
-      @param = {'title'=>ta[:t],'author'=>ta[:a],'location'=>pub[:l],'publisher'=>pub[:p],'year'=>pub[:y],'isbn'=>isbn[1]}
-    end
-  end
+			content=HTMLEntities.new.decode( response.body.force_encoding("UTF-8"))
+			#content=HTMLEntities.new.decode(File.read('example').force_encoding("UTF-8"))
+			ta=/题名与责任.+?<\/td>.+?<td.+?>.+?>.+?>[[:blank:]]*(?<t>.+?)[[:blank:]]*\[.+?\].+?\/[[:blank:]]*(?<a>.+?)[[:blank:]]*[著编]*<.+?<\/td>/m.match(content)
+			pub=/出版项.+?<\/td>.+?<td.+?>.+?>.+?>[[:blank:]]*(?<l>.+?)[[:blank:]]*:[[:blank:]]*(?<p>.+?)[[:blank:]]*,[[:blank:]]*(?<y>.+?)[[:blank:]]*<.+?<\/td>/m.match(content)
+			isbn=/Z13_ISBN_ISSN.+?value=\"(.+?)[[:blank:]]+/.match(content)
+			@param = {'title'=>ta[:t],'author'=>ta[:a],'location'=>pub[:l],'publisher'=>pub[:p],'year'=>pub[:y],'isbn'=>isbn[1]}
+		end
+	end
+	class CNKI
+		def initialize(id, prefix)
+			@hash={"CCND"=>"newspaper", "CDFD"=>"dissertation_D", "CMFD"=>"dissertation_M", "IPFD"=>"conference_I", "CJFQ"=>"journal", "CPFD"=>"conference_C", "CYFD"=>"annual"}
+			@id=id
+			case prefix
+			when nil
+				@db_prefix=nil
+				@type='preprint'
+			else
+				@db_prefix=prefix
+				@type=@hash[prefix]
+			end
+		end
+		def get
+			case @type
+			when 'preprint'
+				uri=URI.parse('http://www.cnki.net/kcms/detail/'+@id)
+			else
+				uri = URI.parse('http://www.cnki.net/kcms/detail/detail.aspx')
+				uri.query=URI.encode_www_form({:FileName=>@id, :DbCode=>@db_prefix})
+			end	
+			response=Net::HTTP::get_response(uri)
+			content=HTMLEntities.new.decode( response.body.force_encoding("UTF-8"))
+			case @type
+			when 'journal'
+				detail=/GetInfo\(.+?,\'(?<year>.+?)\',\'(?<issue>.+?)\',\'(?<journal>.+?)\'\)/.match(content)
+				title=/RegAd\(\'.+?\',\'.+?\',\'.+?\',\'.+?\',\'.+?\',\'(?<title>.+?)\'\)/.match(content)
+				@params={'title'=>title[:title], 'year'=>detail[:year], 'issue'=>detail[:issue], 'journal'=>detail[:journal]}
+			end
+		end
+	end
 end
